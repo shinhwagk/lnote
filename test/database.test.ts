62a3a55ba3b7e6543a9a1c7ffbe84eb7cf7bb621
shinhwagk <shinhwagk@outlook.com>
2019-01-06 14:23:48 +0800

    save

--- test/database.test.ts
+++ test/database.test.ts
@@ -1,11 +1,41 @@
+import * as fs from "fs";
 import * as db from '../src/database';
 
-const vscodeDB = new db.VSNoteDatabase("./test");
+let vscodeDB: db.VSNoteDatabase | undefined = undefined;
+
+const testData = {
+    powershell: {
+        install: {},
+        ".notes": [1, 2, 3]
+    },
+    oracle: {
+        ".notes": []
+    }
+}
+
+beforeAll(() => {
+    fs.writeFileSync("./test/domains.json", JSON.stringify(testData), { encoding: "utf-8" });
+    vscodeDB = new db.VSNoteDatabase("./test");
+
+    fs.mkdirSync("./test/notes");
+    fs.mkdirSync("./test/notes/1");
+    fs.writeFileSync("./test/notes/1/1", "test", { encoding: "utf-8" });
+    fs.writeFileSync("./test/notes/1/2.sql", "select * from dual;", { encoding: "utf-8" });
+});
 
 test('true', () => {
-    expect(vscodeDB.existChildDomain("/powershell")).toBe(true);
+    expect(vscodeDB!.existChildDomain("/powershell")).toBe(true);
 });
 
 test('false', () => {
-    expect(vscodeDB.existChildDomain("/oracle")).toBe(false);
+    expect(vscodeDB!.existChildDomain("/oracle")).toBe(false);
+});
+
+test('dqual powershell notes', () => {
+    expect(vscodeDB!.readNotesIdOfDomain('/powershell')).toEqual(testData.powershell[".notes"]);
+});
+
+afterAll(() => {
+    fs.unlinkSync("./test/domains.json");
+    fs.unlinkSync("./test/notes");
 });
