f41cb22b4272fe85fd48f8ebcabfe375ab82fee1
shinhwagk <shanghai_sl@hotmail.com>
2019-05-03 21:45:46 +0800

    0.9.70

--- upgrade/0.9.68.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs'),
-    path = require('path');
-
-const clientHome = path.join(homedir(), '.vscode-note');
-const clientVersionFile = path.join(clientHome, 'version');
-
-fs.existsSync(clientVersionFile) && fs.unlink(clientVersionFile);
--- upgrade/0.9.70.js
+++ upgrade/0.9.70.js
@@ -1,7 +1,8 @@
 const fs = require('fs'),
+    { homedir } = require('os'),
     path = require('path');
 
 const clientHome = path.join(homedir(), '.vscode-note');
 const clientVersionFile = path.join(clientHome, 'version');
 
-fs.existsSync(clientVersionFile) || fs.unlink(clientVersionFile);
+fs.existsSync(clientVersionFile) && fs.unlinkSync(clientVersionFile);
