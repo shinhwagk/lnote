b97ff6654b8c7b8354beff40121c7d3fd19ed443
shinhwagk <shanghai_sl@hotmail.com>
2019-05-04 12:21:24 +0800

    update 0974

--- upgrade/0.9.74.js
+++ upgrade/0.9.74.js
@@ -4,14 +4,16 @@ const fs = require('fs'),
 
 const clientHome = path.join(homedir(), '.vscode-note');
 
-try {
-    for (const f of fs.readdirSync(clientHome)) {
-        const file = path.join(clientHome, f)
-        if (fs.statSync(file).isFile) {
-            fs.unlinkSync(file);
+if (fs.existsSync(clientHome)) {
+    try {
+        for (const f of fs.readdirSync(clientHome)) {
+            const file = path.join(clientHome, f)
+            if (fs.statSync(file).isFile) {
+                fs.unlinkSync(file);
+            }
         }
+        fs.rmdirSync(clientHome)
+    } catch (e) {
+            console.log(e)
     }
-    fs.rmdirSync(clientHome)
-} catch (e) {
-        console.log(e)
 }
