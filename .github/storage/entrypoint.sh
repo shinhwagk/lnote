808f9123d7f3cbc549990db8cc7dcbd6be597b61
shinhwagk <shanghai_sl@hotmail.com>
2019-05-06 19:37:59 +0800

    update

--- .github/storage/entrypoint.sh
+++ .github/storage/entrypoint.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+git config user.name "${GITHUB_ACTOR}"
+git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+
 git checkout -b analytics origin/analytics
 
 storage
+
+git add storage
+git commit -m "update storage timestamp"
+git push -u origin analytics
