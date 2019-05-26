44c1d947197f69842ac2c9f50aa85590a34a0e06
shinhwagk <shanghai_sl@hotmail.com>
2019-05-26 12:29:56 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,11 +5,11 @@ repository='https://github.com/shinhwagk/vscode-note'
 branch=(vscode-note gh-pages dockerhub@storage2file-firestore dockerhub@storage2file firebase@functions)
 
 for b in ${branch[*]}; do
-	  if [ -d $b ]; then
-		      cd "$b" && git pull &
-		        else
-				    git clone --depth=1 -b "$b" "$repository" "$b" &
-				      fi
-			      done
+  if [ -d $b ]; then
+		cd "$b" && git pull &
+	else
+		git clone --depth=1 -b "$b" "$repository" "$b" &
+	fi
+done
 
-			      wait
+wait
