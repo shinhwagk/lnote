3069caadc52f8e509362e724d743b0f861dd3c34
shinhwagk <shanghai_sl@hotmail.com>
2019-05-27 14:34:44 +0800

    update

--- init.sh
+++ init.sh
@@ -4,10 +4,12 @@ repository='https://github.com/shinhwagk/vscode-note'
 
 branch=(gh-pages dockerhub/storage2file-firestore dockerhub/storage2file firebase/functions analytics npm/storage2file vscode-note)
 
+ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 for b in ${branch[*]}; do
 	echo "process $b"
   if [ -d "$b" ]; then
-		cd $b && git pull && cd ..
+		cd $b && git pull && cd $ROOT
 	else
 		echo "clone repo ${b}"
 		git clone --depth=1 -b "$b" "$repository" "$b"
