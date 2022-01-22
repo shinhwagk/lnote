b8ebb00d3f33276354c2a7c0e58988485c69de84
unknown <shinhwagk@outlook.com>
2022-01-22 12:23:44 +0800

    Update extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -28,7 +28,6 @@ export async function activate(context: ExtensionContext) {
      * note
      */
     ext.registerCommand('vscode-note.note.edit.delete', ExtCmds.cmdHdlNoteEditRemove);
-    ext.registerCommand('vscode-note.note.edit', ExtCmds.cmdHdlNoteEdit);
     ext.registerCommand('vscode-note.note.edit.col', ExtCmds.cmdHdlNoteEditCol);
     ext.registerCommand('vscode-note.note.edit.col.content', ExtCmds.cmdHdlNoteEditColContent);
     ext.registerCommand('vscode-note.note.edit.col.add', ExtCmds.cmdHdlNoteEditColAdd);
@@ -65,4 +64,4 @@ export async function activate(context: ExtensionContext) {
     });
 }
 
-export function deactivate() { }
+export function deactivate() {}
