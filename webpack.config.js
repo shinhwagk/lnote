6a4635746317c63e5736438f2dc9f1a95de5568a
gk <shanghai_sl@hotmail.com>
2022-09-04 14:55:21 +0800

    check

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,42 @@
-const path = require('path')
-const CopyPlugin = require('copy-webpack-plugin')
-
 // @ts-check
+
+const path = require('path');
+const copy = require('copy-webpack-plugin');
+
 /** @typedef {import('webpack').Configuration} WebpackConfig **/
 
 /** @type WebpackConfig */
 const extConfig = {
   target: 'node',
+  mode: 'none',
   entry: './src/extension.ts',
   output: {
+    path: path.resolve(__dirname, 'out'),
     filename: 'extension.js',
-    libraryTarget: 'commonjs2',
-    path: path.resolve(__dirname, 'out')
+    libraryTarget: 'commonjs2'
   },
-  resolve: { extensions: ['.ts', '.js'] },
-  module: { rules: [{ test: /\.ts$/, loader: 'ts-loader', options: { configFile: '../tsconfig.json' } }] },
-  externals: { vscode: 'vscode' }
-}
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader',
+        options: {
+          configFile: '../tsconfig.json'
+        }
+      }
+    ]
+  },
+  externals: {
+    vscode: 'commonjs vscode'
+  },
+  devtool: 'nosources-source-map',
+  infrastructureLogging: {
+    level: "log"
+  },
+};
 
 const webConfig = {
   target: 'web',
@@ -26,10 +46,10 @@ const webConfig = {
     rules: [{ test: /\.ts$/, loader: 'ts-loader', options: { configFile: './tsconfig.json' } }]
   },
   plugins: [
-    new CopyPlugin({
+    new copy({
       patterns: [{ from: './web/main.css' }]
     })
   ]
-}
+};
 
-module.exports = [webConfig, extConfig]
+module.exports = [webConfig, extConfig];
