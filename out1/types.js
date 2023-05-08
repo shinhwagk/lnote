d8ccb0c086eec117f4279424a598deff8d756f9f
gk <shinhwagk@outlook.com>
2023-05-08 15:38:25 +0800

    check

--- out1/types.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// export type NoteDataLabel = { [gl: string]: string[] };
-// export type EditKind = 'NotesSetCommonGroupLabels' | 'NoteData' | 'DomainGroupLabel' = 'DomainGroupLabel'
-//# sourceMappingURL=types.js.map
